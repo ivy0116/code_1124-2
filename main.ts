3401c63ca4b7b899f554ff90a3e53343a62daca2
ivy0116 <71689324+ivy0116@users.noreply.github.com>
2020-11-24 15:15:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
 })
 radio.setGroup(10)
 basic.forever(function () {
